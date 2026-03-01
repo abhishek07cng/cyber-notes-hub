f90f0679a5308d49e471a27c111744cd450d2e6b
Abhishek Kumar Singh <abhisheksingh20590124@gmail.com>
2026-03-02 01:47:50 +0530

    feat: implement JWT authentication with HttpOnly cookies, protect admin routes, and enhance Note schema for structured security research logs

--- server/models/note.js
+++ server/models/note.js
@@ -5,14 +5,38 @@ const noteSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
+  summary: {
+    type: String,
+    required: true,
+  },
+
   content: {
     type: String,
     required: true,
   },
+
   category: {
     type: String,
     required: true,
+  },
+
+  tags: [
+    {
+      type: String
+    }
+  ],
+
+  difficulty: {
+    type: String,
+    enum: ["Beginner", "Intermediate", "Advanced"],
+    default: "Beginner"
+  },
+
+  platform: {
+    type: String
   }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Note", noteSchema);
--- server/server.js
+++ server/server.js
@@ -5,29 +5,40 @@ const Note = require("./models/note");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Admin = require("./models/Admin");
+const protect = require("./middleware/authMiddleware");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173", // frontend URL later
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
-app.post("/api/notes", async (req, res) => {
+app.post("/api/notes", protect, async (req, res) => {
   try {
-    const { title, content, category } = req.body;
+    const { title, summary, content, category, tags, difficulty, platform } =
+      req.body;
     const newNote = new Note({
       title,
+      summary,
       content,
       category,
+      tags,
+      difficulty,
+      platform,
     });
     const savedNote = await newNote.save();
     res.status(201).json(savedNote);
@@ -38,7 +49,7 @@ app.post("/api/notes", async (req, res) => {
 app.get("/api/notes", async (req, res) => {
   try {
     const notes = await Note.find().sort({ createdAt: -1 });
-    res.status(201).json(notes);
+    res.status(200).json(notes);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -54,12 +65,13 @@ app.get("/api/notes/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-app.put("/api/notes/:id", async (req, res) => {
+app.put("/api/notes/:id", protect, async (req, res) => {
   try {
-    const {title, content,category} = req.body ;
+    const { title, summary, content, category, tags, difficulty, platform } =
+      req.body;
     const updateNote = await Note.findByIdAndUpdate(
       req.params.id,
-      {title,content, category },
+      { title, summary, content, category, tags, difficulty, platform },
       { new: true, runValidators: true },
     );
     if (!updateNote) {
@@ -70,38 +82,44 @@ app.put("/api/notes/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-app.delete("/api/notes/:id", async(req,res) => {
-    try {
-        const deleteNote = await Note.findByIdAndDelete(req.params.id);
-        if(!deleteNote){
-            return res.send(404).json({message : "Note not found"});
-        }
-        res.status(200).json({deleteNote});
-    }catch(error){
-        res.status(500).json({message: error.message});
+app.delete("/api/notes/:id", protect, async (req, res) => {
+  try {
+    const deleteNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deleteNote) {
+      return res.status(404).json({ message: "Note not found" });
     }
-})
-app.post("/api/admin/login", async(req, res) => {
-  try{
-    const {email, password} = req.body ;
-    const admin = await Admin.findOne({email});
-    if(!admin){
-      return res.status(400).json({message:"Invalid Credentials"});
-    }
-    const isMatch = await bcrypt.compare(password,admin.password);
-    if(!isMatch){
-        return res.status(400).json({message:"Invalid Credentials"});
-    }
-    const token = jwt.sign(
-      {id  : admin._id},
-      process.env.JWT_SECRET ,
-      {expiresIn :"1d"}
-    )
-    res.status(200).json({token});
-  }catch(error){
-    res.status(500).json({message: error.message});
+    res.status(200).json({ deleteNote });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-})
+});
+app.post("/api/admin/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const admin = await Admin.findOne({ email });
+    if (!admin) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "strict",
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+      })
+      .status(200)
+      .json({ message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
