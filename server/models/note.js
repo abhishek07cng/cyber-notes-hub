b64abfdcf19c03dc8c7e234e66a30eafbf4df241
Abhishek Kumar Singh <abhisheksingh20590124@gmail.com>
2026-03-08 02:34:28 +0530

    fix noteDetails issue

--- server/models/note.js
+++ server/models/note.js
@@ -1,36 +1,56 @@
 const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-
-    summary: {
-      type: String,
-      required: true,
-    },
-
-    category: {
-      type: String,
-      required: true,
-    },
-
-    tags: [String],
-
-    difficulty: {
-      type: String,
-      default: "Beginner",
-    },
-
-    concept: String,
-    payload: String,
-    explanation: String,
-    mitigation: String,
-    references: String,
+{
+  title: {
+    type: String,
+    required: true,
   },
-  { timestamps: true },
+
+  summary: {
+    type: String,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+
+  difficulty: {
+    type: String,
+    default: "Beginner",
+  },
+
+  tags: [String],
+
+  concept: {
+    type: String,
+    default: ""
+  },
+
+  payload: {
+    type: String,
+    default: ""
+  },
+
+  explanation: {
+    type: String,
+    default: ""
+  },
+
+  mitigation: {
+    type: String,
+    default: ""
+  },
+
+  references: {
+    type: String,
+    default: ""
+  }
+
+},
+{ timestamps: true }
 );
 
 module.exports = mongoose.model("Note", noteSchema);
